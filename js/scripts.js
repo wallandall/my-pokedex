f854a553276ab09f4365472b3bca728220a7a09f
wallandall <wallandall@gmail.com>
2019-10-18 19:45:55 +0200

    Initial Data Strucutre

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
-alert("Hello world");
-
-var favouriteFood = "Sushi";
-
-document.write(favouriteFood);
+repository = [];
+repository = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Ivysaur",
+    height: 1,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Venusaur",
+    height: 2,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Charmander",
+    height: 0.6,
+    types: ["fire"],
+  },
+  {
+    name: "Charmeleon",
+    height: 1.1,
+    types: ["fire"],
+  },
+  {
+    name: "Charizard",
+    height: 1.7,
+    types: ["fire", "flying"],
+  },
+];
