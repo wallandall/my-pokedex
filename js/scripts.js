b0a2e3a91fc519887db4b3e08af99eaf3c3596ea
wallandall <wallandall@gmail.com>
2019-10-20 22:00:56 +0200

    Refactor repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,64 @@
-var repository = [];
-repository = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Ivysaur",
-    height: 1,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Venusaur",
-    height: 2,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Charmander",
-    height: 0.6,
-    types: ["fire"],
-  },
-  {
-    name: "Charmeleon",
-    height: 1.1,
-    types: ["fire"],
-  },
-  {
-    name: "Charizard",
-    height: 7,
-    types: ["fire", "flying"],
-  },
-];
+var pokemonRepository = (function() {
+  var repository = [];
 
-repository.forEach(function(currentPokemon) {
+  repository = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Ivysaur",
+      height: 1,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Venusaur",
+      height: 2,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      types: ["fire"],
+    },
+    {
+      name: "Charmeleon",
+      height: 1.1,
+      types: ["fire"],
+    },
+    {
+      name: "Charizard",
+      height: 7,
+      types: ["fire", "flying"],
+    },
+  ];
+
+  function add(pokemon) {
+    if (typeof pokemon === "object") {
+      console.log(Object.keys(pokemon));
+      console.log(Object.keys(repository));
+      repository.push(pokemon);
+    }
+  }
+
+  function getAll() {
+    return repository;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+pokemonRepository.add({
+  name: "Pikachu",
+  height: 0.4,
+  types: ["electric"],
+});
+
+pokemonRepository.getAll().forEach(function(currentPokemon) {
   var pokemon = document.createElement("div");
 
   var pokemonTitle = document.createElement("h1");
