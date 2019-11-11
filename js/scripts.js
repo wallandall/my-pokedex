6ca1a5e58e430e5abefecb099169cd8f50bea99e
wallandall <wallandall@gmail.com>
2019-11-11 19:19:33 +0100

    Add bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -31,7 +31,10 @@ const pokemonRepository = (function() {
         // Now we add the details to the item
         item.imgUrl = details.sprites.front_default;
         item.height = details.height;
-        item.types = Object.keys(details.types);
+        item.types = details.types.map(function(pokemon) {
+          return pokemon.type.name;
+        });
+        item.weight = details.weight;
         isLoading(false);
       })
       .catch(function(e) {
@@ -62,92 +65,43 @@ const pokemonRepository = (function() {
   }
 
   //Render Pokemons to screen
-  function addListItem(pokemon, item) {
-    var $listItem = document.createElement("li");
-    var $pokemonButton = document.createElement("button");
-    $pokemonList.appendChild($listItem);
-    $pokemonButton.innerText = pokemon.name;
-    $pokemonButton.classList.add("pokemon-button");
-    $listItem.appendChild($pokemonButton);
-
-    //Add Event Listner for button click
-    $pokemonButton.addEventListener("click", function() {
+  function addListItem(pokemon) {
+    $listItem = $(
+      '<button type="button" class="list-group-item pokemon-button title" data-toggle="modal" data-target="#pokemon-modal"></button>',
+    );
+    $listItem.text(pokemon.name);
+    $pokemonList.append($listItem);
+    $listItem.click(function() {
       showDetails(pokemon);
     });
   }
 
-  var $modalContainer = document.querySelector("#modal-container");
   //Show Title, text and image in model
-  function showModal(title, text, image) {
-    var modal = document.createElement("div");
-    modal.classList.add("modal");
+  function showModal(title, text, image, weight, types) {
+    $("#pokemon-title").text(title);
+    $(".modal-body")
+      .empty()
+      .append('<img class="pokemon-image" />')
+      .append('<p id="pokemon-height">Height: ' + text + "</p>")
+      .append('<p id="pokemon-weight">Weight: ' + weight + "</p>")
+      .append('<p id="pokemon-types">Type: ' + types + "</p>");
 
-    // Add the new modal content
-    var closeButtonElement = document.createElement("button");
-    closeButtonElement.classList.add("modal-close");
-    closeButtonElement.innerText = "x";
-    closeButtonElement.addEventListener("click", hideModal);
-
-    //Pokemon Name
-    var titleElement = document.createElement("h1");
-    titleElement.innerText = title;
-
-    //Pokemon Height
-    var contentElement = document.createElement("p");
-    contentElement.innerText = "Height: " + text;
-
-    //Pokemon Image
-    var imageElement = document.createElement("img");
-    imageElement.src = image;
-    imageElement.classList.add("pokemon-image");
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modal.appendChild(imageElement);
-    $modalContainer.appendChild(modal);
-    //Make the Modal Visible
-    $modalContainer.classList.add("is-visible");
-  }
-
-  //Hide Modal
-  function hideModal() {
-    $modalContainer.classList.remove("is-visible");
-    // Clear all existing modal content
-    $modalContainer.innerHTML = "";
+    $(".pokemon-image").attr("src", image);
   }
 
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function() {
-      showModal(item.name, item.height, item.imgUrl);
+      showModal(item.name, item.height, item.imgUrl, item.weight, item.types);
     });
   }
 
-  //Hide modal if user hits the esc key
-  window.addEventListener("keydown", e => {
-    if (
-      e.key === "Escape" &&
-      $modalContainer.classList.contains("is-visible")
-    ) {
-      hideModal();
-    }
-  });
-
-  //Hide model if user clicks on the modal overlay
-  $modalContainer.addEventListener("click", e => {
-    var target = e.target;
-    if (target === $modalContainer) {
-      hideModal();
-    }
-  });
-
   function isLoading(loading) {
     if (loading) {
-      $spinner.classList.remove("is-not-loading");
-      $spinner.classList.add("is-loading");
+      $spinner.addClass("is-loading");
+      $spinner.removeClass("is-not-loading");
     } else {
-      $spinner.classList.remove("is-loading");
-      $spinner.classList.add("is-not-loading");
+      $spinner.addClass("is-not-loading");
+      $spinner.removeClass("not-loading");
     }
   }
 
@@ -159,14 +113,12 @@ const pokemonRepository = (function() {
     loadList: loadList,
     loadDetails: loadDetails,
     showModal: showModal,
-    hideModal: hideModal,
   };
 })();
 
-var $pokemonList = document.querySelector(".pokemon-list");
-var $spinner = document.querySelector("#spinner");
+var $pokemonList = $("#pokemon-list");
+var $spinner = $("#spinner");
 
-//var $modalContainer = document.querySelector("#modal-container");
 pokemonRepository.loadList().then(function() {
   // Loop throu all items in list
   pokemonRepository.getAll().forEach(function(pokemon) {
